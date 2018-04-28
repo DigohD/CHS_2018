a15cc991fa18a7fd884823dfd51c1606a3de98d8
Kevin <duappe93@gmail.com>
2018-04-28 18:43:59 +0200

    Added Starry sky

---
+++ CHS_2018/Assets/Script/Objects/Stars.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stars : MonoBehaviour {
+
+    public GameObject P_BGStar;
+
+	// Use this for initialization
+	void Start () {
+
+        for (int i=0; i < 30; i++){
+            GameObject newstar = Instantiate(P_BGStar);
+            float x = Random.Range(-800, 800);
+            float z = Random.Range(-800, 800);
+            float scale = Random.Range(0.5f,8f);
+            float rot = Random.Range(0, 360);
+
+            newstar.transform.position = new Vector3(x, -180, z);
+            newstar.transform.localScale = Vector3.one * scale;
+            newstar.transform.Rotate(0,rot,0);
+        }
+        
+        	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
